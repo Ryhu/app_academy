454b703cee26ec06917ab9f11270d0c7b832e719
TsetenAndRyan <appacademy@Apps-Mac-mini.local>
2015-10-02 17:34:48 -0400

    last touches

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,44 +1,43 @@
-
-
-
-
+require "byebug"
 class Minesweeper
+  attr_reader :board
+
   def initialize(boardX = 9,boardY = 9, bombs = 15)
     @board = Board.new(boardX, boardY, bombs)
-    @win_count
+    @win_count = 0
   end
 
   def reveal(pos)
-    @board[pos[0]][pos[1]].revealed = true
-    p "aiwuhfklsudfh"
+    @board[pos].revealed = true
     @board.render
-    if @board[pos[0]][pos[1]].value == 9
+    if @board[pos].bombed?
       game_over
     else
       @win_count += 1
-      if @board[pos[0]][pos[1]].value == 0
+      if win_check
+        return "YOU WINNNNNN"
+      end
+      if @board[pos].value == 0
         zero_propegate(pos)
-        if win_check
-          return "YOU WINNNNNN"
-        else
-          prompt_user
-        end
+      else
+        prompt_user
       end
     end
   end
+
 
   def zero_propegate(pos)
     CONSTANTS.each do |el|
-      if in_bounds?([el[0] + pos[0] , el[1] + pos[1]])
-        unless @board[el[0] + pos[0]][el[1] + pos[1]].reveal
-          reveal(@board[el[0] + pos[0]][el[1] + pos[1]].position)
+      if self.board.in_bounds?([el[0] + pos[0] , el[1] + pos[1]])
+        unless @board[[el[0] + pos[0], el[1] + pos[1]]].revealed?
+          reveal(@board[[el[0] + pos[0], el[1] + pos[1]]].position)
         end
       end
     end
   end
 
-  def wincheck
-    win_count == boardX * boardY - bombs
+  def win_check
+    @win_count == @board.boardX * @board.boardY - @board.bombs
   end
 
   def flag_bomb(pos)
@@ -52,12 +51,13 @@ class Minesweeper
   end
 
   def prompt_user
+    pos = []
     puts "Enter command"
-    puts "reveal, flag, ping"
+    puts "reveal, flag"
     command = gets.chomp
 
     puts "Enter position in ?,?"
-    pos = gets.chomp
+    pos = gets.chomp.split(',').map(&:to_i)
 
     reader(command,pos)
   end
@@ -88,7 +88,7 @@ class Tile
   def initialize(pos, board)
     @bombed = false
     @flagged = false
-    @revealed = true
+    @revealed = false
     @position = pos
     @value = 0
     tile_board = board
@@ -98,15 +98,37 @@ class Tile
   #   @value =  value
   # end
 
-  def revealed
+  def revealed?
     @revealed
   end
 
+  def revealed= (boolean)
+    @revealed = boolean
+  end
+
+  def bombed?
+    @bombed
+  end
+
+  def bombed= (boolean)
+    @bombed = boolean
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def in_bounds?(pos)
+    pos[0].between?(0,@board.length-1) && pos[1].between?(0,@board[0].length-1)
+  end
 end
 
 
 
+
 class Board
+  attr_reader :boardX, :boardY, :bombs
+
   def initialize(boardX, boardY, bombs)
     @board_size = [boardX,boardY]
     @boardX = boardX
@@ -117,6 +139,11 @@ class Board
     put_numbers
   end
 
+  def [](pos)
+    x,y = pos
+    return @board[x][y]
+  end
+
   def board_maker
     making_board = Array.new(@boardX){Array.new(@boardY)}
     making_board.each_with_index do | row, idx1 |
@@ -124,7 +151,7 @@ class Board
         making_board[idx1][idx2] = Tile.new([idx1,idx2], self)
       end
     end
-      making_board.flatten.sample(@bombs).each{|tile| tile.value = 9}
+      making_board.flatten.sample(@bombs).each{|tile| tile.bombed = true}
       @board = making_board
   end
 
@@ -151,7 +178,7 @@ class Board
     around = 0
     CONSTANTS.each do |el|
       if in_bounds?([el[0] + pos[0] , el[1] + pos[1]])
-        if @board[el[0] + pos[0]][el[1] + pos[1]].value == 9
+        if @board[el[0] + pos[0]][el[1] + pos[1]].bombed? == true
         around += 1
         end
       end
@@ -161,24 +188,27 @@ class Board
 
 
   def render
-    @board.each_with_index do | row, idx1 |
-      row.each_with_index do | col, idx2 |
-        if @board[idx1][idx2].flagged
-          print 'F'
-          if @board[idx1][idx2].revealed
-            if @board[idx1][idx2].value == 0
-              print '_'
-            else
-            print @board[idx1][idx2].value
-            end
-          else
-            print '*'
+        @board.each_with_index do | row, idx1 |
+          row.each_with_index do | col, idx2 |
+                if @board[idx1][idx2].flagged?
+                  print 'F'
+                else
+                      if @board[idx1][idx2].revealed?
+                            if @board[idx1][idx2].value == 0
+                              print '_'
+                            elsif @board[idx1][idx2].bombed?
+                              print 'X'
+                            else
+                            print @board[idx1][idx2].value
+                            end
+                      else
+                      print '*'
+                      end
+                end
+                print ' '
           end
+          puts
         end
-        print ' '
       end
-      puts
-    end
-  end
 
 end
